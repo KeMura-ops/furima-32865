cca774c7bf6682b12359a125ce24a136b52d94a9
KeMura-ops <kenti432@yahoo.co.jp>
2021-02-23 14:00:55 +0900

    バリデーションの修正とマイグレーションの実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,6 @@ class User < ApplicationRecord
     validates :nickname, :birthday
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'Include both letters and numbers' }
     validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: 'Full-width characters' }
-    validates :first_reading, :last_reading, { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
+    validates :first_reading, :last_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
   end
 end
