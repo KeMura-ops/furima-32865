2b876fb16d54be242fc35a7a2c271d70d8db19c6
KeMura-ops <kenti432@yahoo.co.jp>
2021-03-02 17:11:26 +0900

    itemモデルのバリデーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,21 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+  validates :category,      numericality: { other_than: 1 }
+  validates :conditon,      numericality: { other_than: 1 }
+  validates :delivery_fee,  numericality: { other_than: 1 }
+  validates :delivery_area, numericality: { other_than: 1 }
+  validates :delivery_time, numericality: { other_than: 1 }
+  end
+
+  with_options presence: true do
+    validates :name
+    validates :item_desc
+    validates :image
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range" }
+  end
+  
+  validates :price, format: { with: /\A[0-9]+\z/, message: "Half-width number" }
 end
