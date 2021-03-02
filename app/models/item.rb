ec78315f23c583d4698192ed5d3dcbe99e9366dc
KeMura-ops <kenti432@yahoo.co.jp>
2021-03-02 15:37:59 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :conditon
+  belongs_to :delivery_fee
+  belongs_to :delivery_area
+  belongs_to :delivery_time
+
   belongs_to :user
   has_one_attached :image
 end
