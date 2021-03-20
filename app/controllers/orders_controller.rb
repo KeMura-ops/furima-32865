fbd30fb0771be60163972b97e2fb09eaa8b412cc
KeMura-ops <kenti432@yahoo.co.jp>
2021-03-20 12:16:21 +0900

    各マイグレーションファイルの作成と送信されるパラメーターの確認

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,22 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_item, only: [:index, :create]
+
+  def index
+    @item_purchase = ItemPurchase.new
+  end
+
+  def create
+    
+  end
+
+  private
+
+  def order_params
+    params.require(:item_purchase).permit(:postal_code, :delivery_area_id, :municipality, :street_address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 end
