3e56c605223dfb74907091eefcc067e2851403d4
KeMura-ops <kenti432@yahoo.co.jp>
2021-03-31 23:29:09 +0900

    テストコードの記述を変更

--- spec/models/item_purchase_spec.rb
+++ spec/models/item_purchase_spec.rb
@@ -25,57 +25,57 @@ RSpec.describe ItemPurchase, type: :model do
       it 'user_idが存在していないと保存できない' do
         @item_purchase.user_id = ''
         @item_purchase.valid?
-        expect(@item_purchase.errors.full_messages).to include("User can't be blank")
+        expect(@item_purchase.errors.full_messages).to include("Userを入力してください")
       end
       it 'item_idが存在していないと保存できない' do
         @item_purchase.item_id = ''
         @item_purchase.valid?
-        expect(@item_purchase.errors.full_messages).to include("Item can't be blank")
+        expect(@item_purchase.errors.full_messages).to include("Itemを入力してください")
       end
       it 'tokenが存在していないと購入できない' do
         @item_purchase.token = ''
         @item_purchase.valid?
-        expect(@item_purchase.errors.full_messages).to include("Token can't be blank")
+        expect(@item_purchase.errors.full_messages).to include("カード情報を入力してください")
       end
       it 'postal_codeが空では保存できない' do
         @item_purchase.postal_code = ''
         @item_purchase.valid?
-        expect(@item_purchase.errors.full_messages).to include("Postal code can't be blank")
+        expect(@item_purchase.errors.full_messages).to include("郵便番号を入力してください")
       end
       it 'postal_codeにハイフンが存在していないと保存できない' do
         @item_purchase.postal_code = '1234567'
         @item_purchase.valid?
-        expect(@item_purchase.errors.full_messages).to include("Postal code is invalid")
+        expect(@item_purchase.errors.full_messages).to include("郵便番号は不正な値です")
       end
       it 'delivery_area_id(都道府県)が1では保存できない' do
         @item_purchase.delivery_area_id = 1
         @item_purchase.valid?
-        expect(@item_purchase.errors.full_messages).to include("Delivery area must be other than 1")
+        expect(@item_purchase.errors.full_messages).to include("都道府県は1以外の値にしてください")
       end
       it 'municipality(市区町村)が空では保存できない' do
         @item_purchase.municipality = ''
         @item_purchase.valid?
-        expect(@item_purchase.errors.full_messages).to include("Municipality can't be blank")
+        expect(@item_purchase.errors.full_messages).to include("市区町村を入力してください")
       end
       it 'street_address(番地)が空では保存できない' do
         @item_purchase.street_address = ''
         @item_purchase.valid?
-        expect(@item_purchase.errors.full_messages).to include("Street address can't be blank")
+        expect(@item_purchase.errors.full_messages).to include("番地を入力してください")
       end
       it 'phone_numberが空では保存できない' do
         @item_purchase.phone_number = ''
         @item_purchase.valid?
-        expect(@item_purchase.errors.full_messages).to include("Phone number can't be blank")
+        expect(@item_purchase.errors.full_messages).to include("電話番号を入力してください")
       end
       it 'phone_numberが12桁以上では保存できない' do
         @item_purchase.phone_number = '090123456789'
         @item_purchase.valid?
-        expect(@item_purchase.errors.full_messages).to include("Phone number is invalid")
+        expect(@item_purchase.errors.full_messages).to include("電話番号は不正な値です")
       end
       it 'phone_numberが半角数字で入力されていないと保存できない' do
         @item_purchase.phone_number = '０９０１２３４５６７８'
         @item_purchase.valid?
-        expect(@item_purchase.errors.full_messages).to include("Phone number is invalid")
+        expect(@item_purchase.errors.full_messages).to include("電話番号は不正な値です")
       end
     end
   end
